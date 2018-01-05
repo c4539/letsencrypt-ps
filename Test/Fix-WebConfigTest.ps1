c3c823f7ceafb2b7310aa5d403ba383c3bf8b8ca
c4539 <me@c4539.com>
2018-01-05 17:07:24 +0100

    Fix-WebConfig test added

---
+++ Test/Fix-WebConfigTest.ps1
@@ -0,0 +1,5 @@
+$webconfigfilename = ".\Test\web.config"
+[XML] $webconf = Get-Content $webconfigfilename
+$webconf.OuterXml.ToString() | Out-File ".\Test\web_before.config"
+$webconf.configuration.'system.webServer'.handlers.RemoveChild($webconf.configuration.'system.webServer'.handlers)
+$webconf.OuterXml.ToString() | Out-File -Encoding utf8 ".\Test\web_after.config"
