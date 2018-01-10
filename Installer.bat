21994cec9ae3222f1cbdf92f7ebc0f3f6f8d8556
c4539 <me@c4539.com>
2018-01-10 14:20:20 +0100

    Minor fix

--- Installer.bat
+++ Installer.bat
@@ -1,2 +1,2 @@
 rem need the certificate's thumbprint as the first argument
-powershell.exe -ExecutionPolicy Unrestricted -Command "Import-Module WebAdministration; Set-ItemProperty IIS:\SslBindings\* -Name Thumbprint -Value %1"
+powershell.exe -ExecutionPolicy Unrestricted -Command "Import-Module WebAdministration; Get-Item IIS:\SslBindings\* | Where-Object { $_.Store -eq 'WebHosting' } | Set-ItemProperty -Name Thumbprint -Value %1"
